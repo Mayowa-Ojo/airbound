6266b910261268270ea498e43b372ac512403a32
Mayowa-Ojo <mayowaojo.e@gmail.com>
2021-12-31 20:10:57 -0700

    feat: setup database config

---
+++ internal/config/db.go
@@ -0,0 +1,55 @@
+package config
+
+import (
+	"airbound/internal/ent"
+	"airbound/internal/ent/migrate"
+	"airbound/internal/log"
+	"context"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func NewEntClient(cfg *Config) *ent.Client {
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		cfg.DBUser, cfg.DBPassword, cfg.DBHost, cfg.DBPort, cfg.DBName,
+	)
+
+	client, err := ent.Open(cfg.DBDriver, dsn)
+	if err != nil {
+		log.Fatal("[ENT]: error connecting to db %s", err)
+	}
+
+	log.Info("[ENT]: connected to database")
+
+	return client
+}
+
+func SchemaMigrateUp(ctx context.Context, client *ent.Client) {
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatal("[ENT]: error running migration %s", err)
+	}
+}
+
+func SchemaMigrateDown(ctx context.Context, client *ent.Client) {
+	if err := client.Schema.Create(
+		ctx,
+		migrate.WithDropIndex(true),
+		migrate.WithDropColumn(true),
+	); err != nil {
+		log.Fatal("[ENT]: error running migration %s", err)
+	}
+}
+
+func DumpMigrations(ctx context.Context, client *ent.Client) {
+	file, err := os.Create("migrations.sql")
+	if err != nil {
+		log.Fatal("[ENT]: error reading/creating migrations file %s", err)
+	}
+
+	if err := client.Schema.WriteTo(ctx, file); err != nil {
+		log.Fatal("[ENT]: error writing migrations to file %s", err)
+	}
+}
