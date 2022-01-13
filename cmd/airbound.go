b4b7dd58dad958bb054c52c6dd91d91cdf772d86
Mayowa-Ojo <mayowaojo.e@gmail.com>
2022-01-13 08:39:53 -0700

    feat: setup config, ent client, migration

--- cmd/airbound.go
+++ cmd/airbound.go
@@ -1,1 +1,39 @@
 package cmd
+
+import (
+	"airbound/api"
+	"airbound/internal"
+	"airbound/internal/config"
+	"airbound/internal/log"
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// var glbClient *ent.Client
+
+func Execute() {
+	context := context.Background()
+	cfg := config.LoadConfig()
+
+	client := config.NewEntClient(cfg)
+	defer client.Close()
+
+	config.SchemaMigrateUp(context, client)
+
+	router := api.Run(client, cfg)
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", cfg.Port),
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+			log.Error("server error: %s", err)
+		}
+	}()
+
+	internal.GracefulShutdown(srv)
+}
