56303b1df6a94397433ba81cb268693685bc5148
Mayowa-Ojo <mayowaojo.e@gmail.com>
2022-01-14 08:40:15 -0700

    refactor: load config into struct with envconfig

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,35 +2,34 @@ package config
 
 import (
 	"log"
+	"time"
 
 	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
 )
 
 type Config struct {
-	Port       string
-	DBDriver   string
-	DBName     string
-	DBHost     string
-	DBUser     string
-	DBPassword string
-	DBPort     string
+	Port       int           `envconfig:"PORT"`
+	DBDriver   string        `envconfig:"DB_DRIVER"`
+	DBName     string        `envconfig:"DB_NAME"`
+	DBHost     string        `envconfig:"DB_HOST"`
+	DBUser     string        `envconfig:"DB_USER"`
+	DBPassword string        `envconfig:"DB_PASSWORD"`
+	DBPort     int           `envconfig:"DB_PORT"`
+	JwtSecret  string        `envconfig:"JWT_SECRET"`
+	TokenTTL   time.Duration `envconfig:"TOKEN_TTL"`
 }
 
 func LoadConfig() *Config {
-	env, err := godotenv.Read()
-	if err != nil {
+	var config Config
+
+	if err := godotenv.Load(); err != nil {
 		log.Fatalf("[config]: error loading env file %s", err)
 	}
 
-	config := &Config{
-		Port:       env["PORT"],
-		DBDriver:   env["DB_DRIVER"],
-		DBName:     env["DB_NAME"],
-		DBHost:     env["DB_HOST"],
-		DBUser:     env["DB_USER"],
-		DBPassword: env["DB_PASSWORD"],
-		DBPort:     env["DB_PORT"],
+	if err := envconfig.Process("", &config); err != nil {
+		log.Fatalf("[config]: error populating env vars %s", err)
 	}
 
-	return config
+	return &config
 }
