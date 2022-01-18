64f5ded91e1b9ff68a25fcc7ec4df740abc39c7d
Mayowa-Ojo <mayowaojo.e@gmail.com>
2022-01-18 09:33:58 -0700

    feat: define custom errors

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,12 +3,13 @@ package errors
 import "errors"
 
 var (
-	ErrUserAlreadyExists  = errors.New("user already exists")
-	ErrInvalidPassword    = errors.New("password is invalid")
-	ErrIncorrectPassword  = errors.New("password is incorrect")
-	ErrUserDoesNotExist   = errors.New("user does not exist")
-	ErrAccountBlacklisted = errors.New("account has been blacklisted")
-	ErrAccountBlocked     = errors.New("account has been blocked")
-	ErrAccountClosed      = errors.New("account is closed")
-	ErrInvalidPermission  = errors.New("invalid permission")
+	ErrUserAlreadyExists      = errors.New("user already exists")
+	ErrInvalidPassword        = errors.New("password is invalid")
+	ErrIncorrectPassword      = errors.New("password is incorrect")
+	ErrUserDoesNotExist       = errors.New("user does not exist")
+	ErrAccountBlacklisted     = errors.New("account has been blacklisted")
+	ErrAccountBlocked         = errors.New("account has been blocked")
+	ErrAccountClosed          = errors.New("account is closed")
+	ErrInvalidPermission      = errors.New("invalid permission")
+	ErrAccountAlreadyVerified = errors.New("account is already verified")
 )
