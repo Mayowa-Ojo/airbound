30d332132211dd600a4e3e829d9c1bde690d83bb
Mayowa-Ojo <mayowaojo.e@gmail.com>
2022-01-31 23:53:16 -0700

    config: add fields to env config stuct

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,27 +9,34 @@ import (
 )
 
 type Config struct {
-	APIBaseURL             string        `envconfig:"API_BASE_URL"`
-	Port                   int           `envconfig:"PORT"`
-	DBDriver               string        `envconfig:"DB_DRIVER"`
-	DBName                 string        `envconfig:"DB_NAME"`
-	DBHost                 string        `envconfig:"DB_HOST"`
-	DBUser                 string        `envconfig:"DB_USER"`
-	DBPassword             string        `envconfig:"DB_PASSWORD"`
-	DBPort                 int           `envconfig:"DB_PORT"`
-	JwtSecret              string        `envconfig:"JWT_SECRET"`
-	TokenTTL               time.Duration `envconfig:"TOKEN_TTL"`
-	AWSRegion              string        `envconfig:"AWS_REGION"`
-	AWSSESAccessKeyID      string        `envconfig:"AWS_SES_ACCESS_KEY_ID"`
-	AWSSESSecretAccessKey  string        `envconfig:"AWS_SES_SECRET_ACCESS_KEY"`
-	AWSSESARN              string        `envconfig:"AWS_SES_ARN"`
-	EmailFromName          string        `envconfig:"EMAIL_FROM_NAME"`
-	EmailFromAddress       string        `envconfig:"EMAIL_FROM_ADDRESS"`
-	AccountVerificationTTL time.Duration `envconfig:"ACCOUNT_VERIFICATION_TTL"`
-	TwoFaIssuer            string        `envconfig:"TWO_FA_ISSUER"`
-	TwoFaTTL               time.Duration `envconfig:"TWO_FA_TTL"`
-	TwoFaLength            int           `envconfig:"TWO_FA_LENGTH"`
-	MaxAircraftAge         time.Duration `envconfig:"MAX_AIRCRAFT_AGE"`
+	APIBaseURL                  string        `envconfig:"API_BASE_URL"`
+	Port                        int           `envconfig:"PORT"`
+	DBDriver                    string        `envconfig:"DB_DRIVER"`
+	DBName                      string        `envconfig:"DB_NAME"`
+	DBHost                      string        `envconfig:"DB_HOST"`
+	DBUser                      string        `envconfig:"DB_USER"`
+	DBPassword                  string        `envconfig:"DB_PASSWORD"`
+	DBPort                      int           `envconfig:"DB_PORT"`
+	JwtSecret                   string        `envconfig:"JWT_SECRET"`
+	TokenTTL                    time.Duration `envconfig:"TOKEN_TTL"`
+	AWSRegion                   string        `envconfig:"AWS_REGION"`
+	AWSSESAccessKeyID           string        `envconfig:"AWS_SES_ACCESS_KEY_ID"`
+	AWSSESSecretAccessKey       string        `envconfig:"AWS_SES_SECRET_ACCESS_KEY"`
+	AWSSESARN                   string        `envconfig:"AWS_SES_ARN"`
+	AWSSQSAccessKeyID           string        `envconfig:"AWS_SQS_ACCESS_KEY_ID"`
+	AWSSQSSecretAccessKey       string        `envconfig:"AWS_SQS_SECRET_ACCESS_KEY"`
+	AWSSQSReservationsQueueARN  string        `envconfig:"AWS_SQS_RESERVATIONS_QUEUE_ARN"`
+	AWSSQSReservationsQueueURL  string        `envconfig:"AWS_SQS_RESERVATIONS_QUEUE_URL"`
+	AWSSQSReservationsQueueName string        `envconfig:"AWS_SQS_RESERVATIONS_QUEUE_NAME"`
+	AWSSQSDelay                 int           `envconfig:"AWS_SQS_DELAY"`
+	AWSSQSVisibility            int           `envconfig:"AWS_SQS_VISIBILITY"`
+	EmailFromName               string        `envconfig:"EMAIL_FROM_NAME"`
+	EmailFromAddress            string        `envconfig:"EMAIL_FROM_ADDRESS"`
+	AccountVerificationTTL      time.Duration `envconfig:"ACCOUNT_VERIFICATION_TTL"`
+	TwoFaIssuer                 string        `envconfig:"TWO_FA_ISSUER"`
+	TwoFaTTL                    time.Duration `envconfig:"TWO_FA_TTL"`
+	TwoFaLength                 int           `envconfig:"TWO_FA_LENGTH"`
+	MaxAircraftAge              time.Duration `envconfig:"MAX_AIRCRAFT_AGE"`
 }
 
 func LoadConfig() *Config {
