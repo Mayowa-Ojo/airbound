24d28935ae5edb68a5881d8260ede3074f1ca559
Mayowa-Ojo <mayowaojo.e@gmail.com>
2022-02-01 09:18:39 -0700

    feat: register routes + events

--- api/router.go
+++ api/router.go
@@ -4,45 +4,56 @@ import (
 	"airbound/facade"
 	"airbound/handlers/accesscontrol"
 	"airbound/handlers/actors"
+	"airbound/handlers/air"
 	"airbound/handlers/flights"
 	"airbound/internal/config"
 	"airbound/internal/email"
 	"airbound/internal/ent"
-	"airbound/internal/ent/user"
+	"airbound/internal/event"
 	"airbound/internal/log"
 	"airbound/internal/middleware"
+	"airbound/internal/queue"
 	"airbound/internal/rbac"
 	acRepository "airbound/repository/accesscontrol"
 	actorRepository "airbound/repository/actors"
+	airRepository "airbound/repository/air"
 	flightRepository "airbound/repository/flights"
 	idRepository "airbound/repository/identitydetails"
-	"context"
 	"net/http"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ses"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 )
 
-func Run(client *ent.Client, cfg *config.Config, sess *session.Session) *gin.Engine {
+func Run(client *ent.Client, cfg *config.Config, sess, sqss *session.Session) *gin.Engine {
 	router := gin.New()
 
 	router.Use(log.Logger(logrus.New()), gin.Recovery())
 
-	registerRoutes(router, client, cfg, sess)
-
-	// doRapidTest(client)
-	// doRapidSESTest(sess)
+	registerRoutes(router, client, cfg, sess, sqss)
 
 	return router
 }
 
-func registerRoutes(router *gin.Engine, client *ent.Client, cfg *config.Config, sess *session.Session) {
+func registerRoutes(router *gin.Engine, client *ent.Client, cfg *config.Config, sess, sqss *session.Session) {
+	emitter := event.NewEventEmitter()
 	emailService := email.NewEmailService(sess, cfg)
+	queueService := queue.NewQueueService(sqss, cfg, emitter)
+
+	// queueService.SendFlightReservationMessage(context.Background(), &flightRepository.FlightReservation{
+	// 	ID:                uuid.MustParse("6cc46f7a-b0be-4dbf-8068-0dafa5769d7c"),
+	// 	ReservationNumber: "1F6nvRT5mYvi1KxKe63fuRxAZ8zu3L8vDq",
+	// 	ReservationStatus: enums.ReservationStatusRequested,
+	// 	FlightInstance: &flightRepository.FlightInstance{
+	// 		ID: uuid.MustParse("e353e222-1cce-4bca-aa8e-ec4a334fa906"),
+	// 	},
+	// }, uuid.MustParse("86659acf-770d-4cb3-80b3-cced65280495"))
 
 	flightRepo := flightRepository.NewFlightRepository(client)
+	flightInstanceRepo := flightRepository.NewFlightInstanceRepository(client)
+	flightScheduleRepo := flightRepository.NewFlightScheduleRepository(client)
+	flightReservationRepo := flightRepository.NewFlightReservationRepository(client)
 	userRepo := actorRepository.NewUserRepository(client)
 	adminRepo := actorRepository.NewAdminRepository(client)
 	customerRepo := actorRepository.NewCustomerRepository(client)
@@ -53,17 +64,26 @@ func registerRoutes(router *gin.Engine, client *ent.Client, cfg *config.Config, 
 	addressRepo := idRepository.NewAddressRepository(client)
 	roleRepo := acRepository.NewRoleRepository(client)
 	permissionRepo := acRepository.NewPermissionRepository(client)
+	airlineRepo := airRepository.NewAirlineRepository(client)
+	airportRepo := airRepository.NewAirportRepository(client)
+	aircraftRepo := airRepository.NewAircraftRepository(client)
 
-	flightHandler := flights.NewFlightHandler(flightRepo, cfg, emailService)
+	flightHandler := flights.NewFlightHandler(
+		flightRepo, flightInstanceRepo, flightScheduleRepo, flightReservationRepo, cfg, emailService, queueService,
+	)
 	actorHandler := actors.NewActorHandler(
 		userRepo, adminRepo, customerRepo, pilotRepo, crewRepo,
 		frontDeskRepo, accountRepo, addressRepo, roleRepo, cfg, emailService,
 	)
 	accessControlHandler := accesscontrol.NewAccessControlHandler(roleRepo, permissionRepo, cfg, emailService)
+	airHandler := air.NewAirHandler(airlineRepo, airportRepo, aircraftRepo, addressRepo, cfg)
 
 	flightFacade := facade.NewFlightFacade(flightHandler)
 	actorFacade := facade.NewActorFacade(*actorHandler)
 	accessControlFacade := facade.NewAccessControlFacade(*accessControlHandler)
+	airFacade := facade.NewAirFacade(airHandler)
+
+	emitter.Register(event.CONFIRM_FLIGHT_RESERVATION, &flights.ConfirmReservationEvent{Fh: flightHandler})
 
 	router.LoadHTMLGlob("templates/*")
 
@@ -93,44 +113,16 @@ func registerRoutes(router *gin.Engine, client *ent.Client, cfg *config.Config, 
 
 		v1.Use(middleware.AuthenticateUser(cfg))
 		v1.Use(middleware.EnsureTwoFa())
+		v1.Use(middleware.CheckAccountStatus())
 
 		v1.GET("/roles/:role-id", middleware.AuthorizeUser(rbac.AccessControl.GetRole), accessControlFacade.GetRole)
 
 		v1.POST("/roles", middleware.AuthorizeUser(rbac.AccessControl.CreateRole), accessControlFacade.CreateRole)
 		v1.POST("/permissions", middleware.AuthorizeUser(rbac.AccessControl.CreatePermission), accessControlFacade.CreatePermission)
+		v1.POST("/airlines/register", middleware.AuthorizeUser(rbac.Air.RegisterAirline), airFacade.RegisterAirline)
+		v1.POST("/airports/register", middleware.AuthorizeUser(rbac.Air.RegisterAirport), airFacade.RegisterAirport)
+		v1.POST("/aircrafts/register", middleware.AuthorizeUser(rbac.Air.RegisterAircraft), airFacade.RegisterAircraft)
 
 		v1.PATCH("/roles/:role-id/grant", middleware.AuthorizeUser(rbac.AccessControl.GrantPermission), accessControlFacade.GrantPermission)
 	}
-}
-
-func doRapidTest(c *ent.Client) {
-	ctx := context.Background()
-	user, err := c.User.
-		Query().
-		Where(user.LastnameEQ("Brady")).
-		Only(ctx)
-	if err != nil {
-		log.Fatal("[Rapid-Test]: %s", err)
-	}
-
-	account, err := user.QueryAccount().Only(ctx)
-	if err != nil {
-		log.Fatal("[Rapid-Test]: %s", err)
-	}
-	user.Edges.Account = account
-
-	log.Info("[DEBUG]: User -- %+v", user.Edges.Account)
-}
-
-func doRapidSESTest(sess *session.Session) {
-	srv := ses.New(sess)
-
-	res, err := srv.ListIdentities(&ses.ListIdentitiesInput{
-		IdentityType: aws.String("EmailAddress"),
-	})
-	if err != nil {
-		log.Fatal("[Rapid-Test-SES]: %s", err)
-	}
-
-	log.Info("[Rapid-Test-SES]: result - %+v", *res.Identities[0])
 }
